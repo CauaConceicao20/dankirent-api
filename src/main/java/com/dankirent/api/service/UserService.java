26073812cd72e7f2bf039302a38515d5b7ba103c
Cauã Conceição <cauaconceicaodev@hotmail.com>
2026-01-08 17:03:12 -0300

    refactor: Refactored user creation method

--- src/main/java/com/dankirent/api/service/UserService.java
+++ src/main/java/com/dankirent/api/service/UserService.java
@@ -18,7 +18,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class UserService implements CrudOperations<User> {
-
     private final UserRepository repository;
     private final PhotoService photoService;
     private final StorageService storageService;
@@ -27,23 +26,12 @@ public class UserService implements CrudOperations<User> {
     @Override
     @Transactional
     public User create(User user) {
-        final String DEFAULT_IMAGE_NAME = "default_user_photo.png";
-        final String DEFAULT_CONTENT_TYPE = "image/png";
-
         try {
-            FileMetaData metaData = storageService.getMetaData(DEFAULT_IMAGE_NAME);
-            Group group = groupService.getByName("USER");
-
-            UserGroup userGroup = new UserGroup(group, user);
-            user.getUserGroups().add(userGroup);
+            assignDefaultGroup(user);
             repository.save(user);
-
-            Photo photo = new Photo(null, user, metaData.getFileName(), DEFAULT_CONTENT_TYPE , metaData.getSize(), metaData.getCreatedAt());
-            photoService.create(photo);
-
+            assignDefaultPhoto(user);
             return user;
-
-        } catch(IOException e) {
+        } catch (IOException e) {
             throw new StorageException("Falha ao ler metadados do arquivo");
         }
     }
@@ -65,6 +53,19 @@ public class UserService implements CrudOperations<User> {
 
     @Override
     public void delete(Long id) {
+    }
 
+    private void assignDefaultGroup(User user) {
+        Group group = groupService.getByName("USER");
+        UserGroup userGroup = new UserGroup(group, user);
+        user.getUserGroups().add(userGroup);
+    }
+
+    private void assignDefaultPhoto(User user) throws IOException {
+        final String DEFAULT_IMAGE_NAME = "default_user_photo.png";
+        final String DEFAULT_CONTENT_TYPE = "image/png";
+        FileMetaData metaData = storageService.getMetaData(DEFAULT_IMAGE_NAME);
+        Photo photo = new Photo(null, user, metaData.getFileName(), DEFAULT_CONTENT_TYPE, metaData.getSize(), metaData.getCreatedAt());
+        photoService.create(photo);
     }
 }
